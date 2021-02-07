49d593d2a4eb11ea46b1618cba8a4e77a5a7a06c
CabAlexis <alexis.cabillic@gmail.com>
2021-02-08 00:24:08 +0100

    Endpoints terminés, refacto et validation a faire

--- src/Entity/Category.php
+++ src/Entity/Category.php
@@ -6,6 +6,7 @@ use App\Repository\CategoryRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=CategoryRepository::class)
@@ -14,13 +15,18 @@ class Category
 {
     /**
      * @ORM\Id
-     * @ORM\GeneratedValue
+     * @ORM\GeneratedValue()
      * @ORM\Column(type="integer")
      */
     private $id;
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Assert\Type(
+     *         type="string",
+     *         message="The value {{ value }} is not a valid {{ type }}."         
+     * )
+     * @Assert\NotBlank(message="Le titre est obligatoire")
      */
     private $title;
 
